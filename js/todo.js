bb6b197826e3475043175ddc2a3a1de53eb6625d
kd02109 <57277708+kd02109@users.noreply.github.com>
2022-08-24 02:01:36 +0900

    chang todo into form tag

--- js/todo.js
+++ js/todo.js
@@ -1,58 +1,55 @@
-const todo = document.querySelector(".todo-name__write input");
-const todoBtn = document.querySelector(".todo-name__write button");
+const todo = document.querySelector(".todo-name__write");
+const todoInput = document.querySelector(".todo-name__write input");
 const todoList = document.querySelector(".todo__list");
-
-let todos =[];
+console.log(todo);
+let toDos = [];
 const TODO_KEY = "todos";
 
-function saveTodo(){
-    localStorage.setItem(TODO_KEY,JSON.stringify(todos));
+function saveTodo() {
+  localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
 }
 
-function writeNewtodo(event){
-    const todoValue = todo.value;
-    todo.value = "";
-    const newTodoDic ={
-        text: todoValue,
-        id: Date.now(),
-    };
-    todos.push(newTodoDic);
-    paintTodo(newTodoDic);
-    saveTodo();
+function writeNewtodo(event) {
+  event.preventDefault();
+  const todoValue = todoInput.value;
+  todoInput.value = "";
+  const newTodoDic = {
+    text: todoValue,
+    id: Date.now(),
+  };
+  toDos.push(newTodoDic);
+  paintTodo(newTodoDic);
+  saveTodo();
 }
 
-function paintTodo(todo){
-    const list = document.createElement("li");
-    const span = document.createElement("span");
-    const button = document.createElement("button");
-    list.id = todo.id;
-    span.innerText = todo.text;
-    button.innerText = "❌";
-    button.classList.add("delete");
-    span.classList.add("todo__list")
-    list.appendChild(span);
-    list.appendChild(button);
-    todoList.appendChild(list);
-
-   button.addEventListener("click",deleteTodo);
+function paintTodo(newTodo) {
+  console.log(newTodo);
+  const list = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  list.id = newTodo.id;
+  span.innerText = newTodo.text;
+  button.innerText = "❌";
+  button.classList.add("delete");
+  span.classList.add("todo__list");
+  list.appendChild(span);
+  list.appendChild(button);
+  todoList.appendChild(list);
+  button.addEventListener("click", deleteTodo);
 }
 
-function deleteTodo(event){
-    const li = event.target.parentElement;
-    li.remove();
-    todos= todos.filter((list)=>list.id !== parseInt(li.id));
-    saveTodo();
-} 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((list) => list.id !== parseInt(li.id));
+  saveTodo();
+}
 
 const localStorageTodo = localStorage.getItem(TODO_KEY);
 
-if(saveTodo !== null){
-    const arrayTodo = JSON.parse(localStorageTodo);
-    arrayTodo.forEach(paintTodo);
+if (saveTodo !== null) {
+  const arrayTodo = JSON.parse(localStorageTodo);
+  arrayTodo.forEach(paintTodo);
 }
 
-
-todoBtn.addEventListener("click", writeNewtodo);
-
-
-
+todo.addEventListener("submit", writeNewtodo);
